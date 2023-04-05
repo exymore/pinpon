b8181944c6df97a09000e9668d5954e486564080
Mikita Tserakhau <exymore@yandex.by>
2023-04-05 23:52:52 +0300

    arch: packed app into modules

--- keyboardListeners.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('keydown', function (e) {
-    playerRocket.setMoving(e.code, true);
-});
-
-window.addEventListener('keyup', function (e) {
-    playerRocket.setMoving(e.code, false);
-});
